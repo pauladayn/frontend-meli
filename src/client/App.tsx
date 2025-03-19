f4aa2cc9b0daa31111fd68b80ca9b0d3ceb49e3f
Paula Dayup Nieto <paudayup@gmail.com>
2025-03-19 02:09:03 -0300

    feat: add common style rules, layout, colors, and styles for the header

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,61 +1,83 @@
-import "./App.css";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import logo from "./assets/Logo_ML.png";
+import searchIcon from "./assets/ic_Search.png";
+import "./styles/main.scss";
 
 function App() {
-    const [count, setCount] = useState(0);
+    // const handleLogin = async () => {
+    //     if (process.env.NODE_ENV === "development") {
+    //         const accessToken = process.env.ACCESS_TOKEN;
+    //         console.log("Using hardcoded token in development:", accessToken);
 
-    const handleLogin = async () => {
-      if (process.env.NODE_ENV === "development") {
-          const accessToken = process.env.ACCESS_TOKEN;
-          console.log('Using hardcoded token in development:', accessToken);
-  
-          document.cookie = `access_token=${accessToken}; path=/; max-age=3600`;
-          window.location.href = '/';
-      } else {
-          try {
-              const response = await fetch('https://frontend-meli.onrender.com/auth/meli', {
-                  method: 'GET',
-              });
-              const data = await response.json();
-              console.log('data', data);
-  
-              if (data.redirectUrl) {
-                  window.location.href = data.redirectUrl;
-              }
-          } catch (error) {
-              console.error('error', error);
-          }
-      }
-  };
+    //         document.cookie = `access_token=${accessToken}; path=/; max-age=3600`;
+    //         window.location.href = "/";
+    //     } else {
+    //         try {
+    //             const response = await fetch(
+    //                 "https://frontend-meli.onrender.com/auth/meli",
+    //                 {
+    //                     method: "GET",
+    //                 }
+    //             );
+    //             const data = await response.json();
+    //             console.log("data", data);
+
+    //             if (data.redirectUrl) {
+    //                 window.location.href = data.redirectUrl;
+    //             }
+    //         } catch (error) {
+    //             console.error("error", error);
+    //         }
+    //     }
+    // };
 
     return (
         <>
-            <div>
-                <a href='https://vite.dev' target='_blank'>
-                    <img src='/vite.svg' className='logo' alt='Vite logo' />
-                </a>
-                <a href='https://reactjs.org' target='_blank'>
-                    <img
-                        src={reactLogo}
-                        className='logo react'
-                        alt='React logo'
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className='card'>
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
-            <button onClick={handleLogin}>Login with Mercado Libre</button>
+            <header className='header flex-wrap center'>
+                <nav
+                    className='navbar flex-wrap center'
+                    aria-label='navegación principal'
+                >
+                    <div className='nav-content center'>
+                        <a href='/' aria-label='Mercado Libre'>
+                            <img src={logo} alt='Mercado Libre logo' title='Mercado Libre' />
+                        </a>
+                        <form action='' className='search' role='search' method="GET">
+                            <label
+                                htmlFor='search-input'
+                                className='visually-hidden'
+                            >
+                                Buscar productos
+                            </label>
+                            <input
+                              id="search-input"
+                              name="search"
+                              type='text'
+                              placeholder='Nunca dejes de buscar'
+                              autoComplete="off"
+                            />
+                            <button
+                                className=''
+                                aria-label='Buscar'
+                                type='submit'
+                            >
+                                <img src={searchIcon} alt='' />
+                            </button>
+                        </form>
+                    </div>
+                </nav>
+            </header>
+            <main>
+                <div className='container'>
+                    <div className='row'>
+                        <div className='col-6'>
+                            <p>columna 1</p>
+                        </div>
+                        <div className='col-6'>
+                            <p>columna 2</p>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </>
     );
 }
