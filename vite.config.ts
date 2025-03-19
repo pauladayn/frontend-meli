c4c87893c51ac269f2ebb8f96e052a5c95c0525c
Paula Dayup Nieto <paudayup@gmail.com>
2025-03-19 13:41:30 -0300

    chore: added alias import paths to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || '/frontend-meli',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/client")
+    }
+  }
 })
