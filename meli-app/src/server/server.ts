8972e5e7818e19b808cb9dc832305570d3b38c68
Paula Dayup Nieto <paudayup@gmail.com>
2025-03-21 20:12:29 -0300

    fix: added PORT as enviornment variable for render

--- meli-app/src/server/server.ts
+++ meli-app/src/server/server.ts
@@ -22,7 +22,7 @@ import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const isProduction = process.env.NODE_ENV === "production";
-const port = 3000;
+const port = process.env.PORT || 3000;
 const base = process.env.BASE || "/";
 const app = express();
 
