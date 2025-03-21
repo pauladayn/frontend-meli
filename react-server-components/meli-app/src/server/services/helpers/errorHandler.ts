0fb4f8c8fd6d71cffe749bd7398631f6baab570a
Paula Dayup Nieto <paudayup@gmail.com>
2025-03-21 01:05:10 -0300

    chore: move already implemented files

---
+++ react-server-components/meli-app/src/server/services/helpers/errorHandler.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+interface CustomError extends Error {
+    statusCode?: number;
+    message: string;
+    stack?: string;
+}
+const errorHandler = (error: CustomError, req: express.Request, res: express.Response) => {
+    const statusCode = error.statusCode || 500;
+    const message = error.message || "Internal Server Error";
+
+    res.status(statusCode).json({
+        success: false,
+        message,
+        ...(process.env.NODE_ENV === "development" && error.stack ? { stack: error.stack } : {}),
+    });
+};
+
+export default errorHandler;
---
+++ react-server-components/meli-app/src/server/utils/items.ts
@@ -0,0 +1,47 @@
+import { CategoryDTO, GenericItem, ItemsList, ItemDescription, ItemDetails, Item } from "../../types";
+import { AUTHOR, DECIMALS_QUANTITY } from "./constants";
+
+export const sanitizeCategories = (filters: CategoryDTO[] | Array<{ [key: string]: string }>): Array<string> => {
+  const uniqueCategories = new Set<string>();
+  filters.forEach(filter => {
+    if (Array.isArray(filter.values)) {
+      const valueName = filter.values.find(value => value.name)?.name || "";
+      if (valueName) uniqueCategories.add(valueName);
+    } else {
+      if (filter.name) uniqueCategories.add(filter.name);
+    }
+  });
+  return Array.from(uniqueCategories);
+};
+
+const sanitizeItem = <T extends GenericItem>(
+  item: T,
+): Item => ({
+  id: item.id,
+  title: item.title,
+  price: {
+    amount: item.price,
+    currency: item.currency_id,
+    decimals: DECIMALS_QUANTITY,
+  },
+  picture: item.thumbnail || "",
+  condition: item.condition || "",
+  free_shipping: item.shipping?.free_shipping || false,
+  seller_address: item.seller_address?.state.name || ''
+});
+
+const sanitizeItemsList = <T extends GenericItem>(items: T[], filters: CategoryDTO[]): ItemsList => {
+  return ({
+    author: AUTHOR,
+    items: items.map((item) => sanitizeItem(item)),
+    categories: sanitizeCategories(filters)
+
+  })
+};
+
+const sanitizeItemDetails = <T extends GenericItem>(singleItem: T, itemDescription: ItemDescription): ItemDetails => ({
+  author: AUTHOR,
+  item: { ...sanitizeItem(singleItem), description: itemDescription.plain_text, sold_quantity: singleItem.sold_quantity }
+})
+
+export { sanitizeItemsList, sanitizeItemDetails };
